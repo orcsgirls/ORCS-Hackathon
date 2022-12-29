385e6c402c9dacf3c942546573d935e20e011aac
Ada <59454160+apzzd@users.noreply.github.com>
2022-12-29 13:08:03 -0700

    Update Ski-Bot-Controller.py

--- Ada/Ski-Bot-Controller.py
+++ Ada/Ski-Bot-Controller.py
@@ -33,22 +33,17 @@ class Robot():
         self.motor_wheel.value = 0
     
     def turnleft(self):
-        self.motor_steer.value = 1
-        self.motor_wheel.value = 0.5
-        time.sleep(0.5)
-        self.motor_steer.value = 0
-        self.motor_wheel.value = 0
+        self.motor_steer.value = 0.5
+        self.motor_wheel.value = 0.1
         
     def turnright(self):
-        self.motor_steer.value = -1
-        self.motor_wheel.value = 0.5
-        time.sleep(0.5)
-        self.motor_steer.value = 0
-        self.motor_wheel.value = 0
+        self.motor_steer.value = -0.5
+        self.motor_wheel.value = 0.1
      
  # make the bot
 robot = Robot()
 
+# create video
 # create video
 video = widgets.HTML(value="<iframe src='http://orcspi-vis.local:4664' width=450 height=380>")
 display(video)
@@ -60,34 +55,30 @@ forward_button = widgets.Button(description='forward', layout=button_layout)
 backward_button = widgets.Button(description='backward', layout=button_layout)
 left_button = widgets.Button(description='left', layout=button_layout)
 right_button = widgets.Button(description='right', layout=button_layout)
+creep_button = widgets.Button(description='creep', layout=button_layout)
 
 # display buttons
 middle_box = widgets.HBox([left_button, stop_button, right_button], layout=widgets.Layout(align_self='center'))
-controls_box = widgets.VBox([forward_button, middle_box, backward_button])
+controls_box = widgets.VBox([forward_button, middle_box, backward_button, creep_button])
 display(controls_box)
 
 def stop(change):
     robot.stop()
     
 def step_forward(change):
-    robot.forward(0.4)
-    time.sleep(0.5)
-    robot.stop()
-
+    robot.forward(1)
+    
 def step_backward(change):
-    robot.backward(0.4)
-    time.sleep(0.5)
-    robot.stop()
+    robot.backward(0.5)
 
 def step_left(change):
     robot.turnleft()
-    time.sleep(0.5)
-    robot.stop()
-
+    
 def step_right(change):
     robot.turnright()
-    time.sleep(0.5)
-    robot.stop()
+    
+def creep(change):
+    robot.forward(0.1)
 
 # link buttons to actions
 stop_button.on_click(stop)
@@ -95,3 +86,4 @@ forward_button.on_click(step_forward)
 backward_button.on_click(step_backward)
 left_button.on_click(step_left)
 right_button.on_click(step_right)
+creep_button.on_click(creep)
